42bf6ffc67e18071993508be5d38659095bbcff2
Guillaume Friloux <gfriloux@datasolution.fr>
2026-02-11 16:47:41 +0100

    feat(mkdocs): better linter config
    better linter config
    
    Signed-off-by: Guillaume Friloux <gfriloux@datasolution.fr>

--- templates/mkdocs/checks/rumdl/default.nix
+++ templates/mkdocs/checks/rumdl/default.nix
@@ -4,6 +4,6 @@ pkgs.runCommand "check-rumdl" {
   buildInputs = [pkgs.rumdl];
 } ''
   echo "→ Running rumdl"
-  rumdl check -d "MD046,MD005,MD013,MD031,MD007,MD038" $src
+  rumdl check -d "MD046,MD005,MD013,MD031,MD007,MD038,MD034" --exclude ".venv" $src
   touch $out
 ''
